75673bd2661b0e6069b9471eccbc5579fc30895a
Leonardo Serra <leonardotreze@hotmail.com>
2018-12-05 13:15:05 -0200

    melhorias

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 // server.js
 // set up ===========================================================
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 app.set('port', (process.env.PORT || 3001));
+app.use(bodyParser.urlencoded({ 'extended': 'true' }));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }))
 
 // routes ===========================================================
 app.post('/api/teste', function (req, res) {
-    res.json({"Conteudo": "alow"});
+    var enviado = "";
+
+    if (req.body.bar == 'undefined')
+    {
+        enviado = req.body.bar;
+    }
+    else
+    {
+        enviado = req.body.Bar;
+    }
+
+    res.json({"Conteudo": "alow", "Enviado": enviado});
 });
 
 app.get('/', function(req, res) {
